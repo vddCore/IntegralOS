02b9e24c27f533e08e1a7e23443a0293e0f43bca
vddCore <vddcore@outlook.com>
2023-01-12 11:35:20 +0100

    Added PIT frequency regulation.

--- drivers/io/8259a/pit.c
+++ drivers/io/8259a/pit.c
@@ -5,6 +5,7 @@
  * * * * */
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <string.h>
 #include <hal/irq.h>
 #include <io/port_io.h>
@@ -12,29 +13,45 @@
 
 static void pit_internal_callback(irq_info_t *irq_info);
 
+static uint16_t current_frequency = 18;
 static uint32_t total_ticks = 0;
 static pit_callback_ptr pit_callback = 0;
 
 void pit_init(void) {
-	irq_set_handler(IRQ_NUMBER_PIT, (uintptr_t)&pit_internal_callback);
+    irq_set_handler(IRQ_NUMBER_PIT, (uintptr_t)&pit_internal_callback);
 }
 
 void pit_set_callback(uintptr_t address) {
-	pit_callback = (pit_callback_ptr)address;
+    pit_callback = (pit_callback_ptr)address;
 }
 
 void pit_unset_callback(void) {
-	pit_callback = 0;
+    pit_callback = 0;
+}
+
+void pit_set_frequency(uint16_t frequency) {
+    uint32_t divisor = 1193180 / frequency;
+
+    uint8_t command = PIT_CMD_MODE_SQUARE_WAVE | PIT_CMD_RW_MODE_READ_LSB_MSB;
+    outb(PIT_COMMAND_PORT, command);
+    outb(PIT_CHANNEL0_PORT, divisor & 0xFF);
+    outb(PIT_CHANNEL0_PORT, divisor >> 8);
+
+    current_frequency = frequency;
+}
+
+uint16_t pit_get_current_frequency(void) {
+    return current_frequency;
 }
 
 uint32_t pit_get_total_ticks(void) {
-	return total_ticks;
+    return total_ticks;
 }
 
 static void pit_internal_callback(irq_info_t *irq_info) {
-	total_ticks++;
+    total_ticks++;
 
-	if(pit_callback) {
-		(*(pit_callback))();
-	}
+    if(pit_callback) {
+        (*(pit_callback))();
+    }
 }
--- include/io/8259a/pit.h
+++ include/io/8259a/pit.h
@@ -7,16 +7,32 @@
 #ifndef INCLUDE_IO_8259A_PIT_H_
 #define INCLUDE_IO_8259A_PIT_H_
 
+#include <stdint.h>
+
 #define PIT_CHANNEL0_PORT 0x40
 #define PIT_CHANNEL1_PORT 0x41
 #define PIT_CHANNEL2_PORT 0x42
 #define PIT_COMMAND_PORT  0x43 /* NOTE: This is write-only, read is ignored. */
+
+#define PIT_CMD_BCD                          0x0001
+#define PIT_CMD_MODE_HARDWARE_RETRIG_ONESHOT 0x0002
+#define PIT_CMD_MODE_RATE_GENERATOR          0x0004
+#define PIT_CMD_MODE_SQUARE_WAVE             0x0006
+#define PIT_CMD_MODE_SOFTWARE_STROBE         0x0008
+#define PIT_CMD_MODE_HARDWARE_STROBE         0x000A
+#define PIT_CMD_RW_MODE_READ_LSB             0x0010
+#define PIT_CMD_RW_MODE_READ_MSB             0x0020
+#define PIT_CMD_RW_MODE_READ_LSB_MSB         0x0030 /* LSB, then MSB */
+#define PIT_CMD_COUNTER_1                    0x0040
+#define PIT_CMD_COUNTER_2                    0x0080
 
 typedef void (* pit_callback_ptr)(void);
 
 void pit_init(void);
 void pit_set_callback(uintptr_t address);
 void pit_unset_callback(void);
+void pit_set_frequency(uint16_t frequency);
+uint16_t pit_get_current_frequency(void);
 uint32_t pit_get_total_ticks(void);
 
 #endif /* INCLUDE_IO_8259A_PIT_H_ */
