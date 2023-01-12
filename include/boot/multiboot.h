0e361e85a4d33a8966545d027cb64d4d90c3ce20
vddCore <vddcore@outlook.com>
2023-01-12 11:35:20 +0100

    Kernel now grabs multiboot info from the stack.

---
+++ include/boot/multiboot.h
@@ -0,0 +1,69 @@
+/*
+ * File name: multiboot.h
+ * Description: The Multiboot standard header.
+ *
+ * * * */
+
+#ifndef MULTIBOOT_H_
+#define MULTIBOOT_H_
+
+#define MULTIBOOT_BOOTLOADER_MAGIC 0x2BADB002
+
+typedef struct multiboot_header {
+    uint32_t magic;
+    uint32_t flags;
+    uint32_t checksum;
+    uint32_t header_addr;
+    uint32_t load_addr;
+    uint32_t load_end_addr;
+    uint32_t bss_end_addr;
+    uint32_t entry_addr;
+} multiboot_header_t;
+
+typedef struct aout_symbol_table {
+    uint32_t tab_size;
+    uint32_t str_size;
+    uint32_t address;
+    uint32_t reserved;
+} aout_symbol_table_t;
+
+typedef struct elf_section_header_table {
+    uint32_t num;
+    uint32_t size;
+    uint32_t address;
+    uint32_t shndx;
+} elf_section_header_table_t;
+
+typedef struct multiboot_info {
+    uint32_t flags;
+    uint32_t memory_lower;
+    uint32_t memory_upper;
+    uint32_t boot_device;
+    uint32_t command_line;
+    uint32_t modules_count;
+    uint32_t modules_address;
+    union {
+        aout_symbol_table_t aout_symbols;
+        elf_section_header_table_t elf_section_table;
+    } additional_info;
+    uint32_t memory_map_length;
+    uint32_t memory_map_address;
+} multiboot_info_t;
+
+typedef struct multiboot_module {
+    uint32_t module_start;
+    uint32_t module_end;
+    uint32_t string;
+    uint32_t reserved;
+} multiboot_module_t;
+
+typedef struct memory_map {
+    uint32_t size;
+    uint32_t base_address_low;
+    uint32_t base_address_high;
+    uint32_t length_low;
+    uint32_t length_high;
+    uint32_t type;
+} memory_map_t;
+
+#endif /* MULTIBOOT_H_ */
--- kernel.c
+++ kernel.c
@@ -7,11 +7,18 @@
 #include <stdint.h>
 #include <string.h>
 
+#include <boot/multiboot.h>
 #include <integral/kernel.h>
 #include <integral/tty.h>
 
-void kernel_init(void) {
+void kernel_init(multiboot_info_t *multiboot_info, uint32_t bootloader_magic) {
     init_terminal();
+
+    if(bootloader_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        write("[\\[4BOOT FAILURE\\X] Not loaded by a multiboot-compliant bootloader.");
+        return;
+    }
+
     write_line("This is a terminal test.\n\n\nMeow.");
 
     return;
