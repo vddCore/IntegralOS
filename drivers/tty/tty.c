5f26ec601781f98c669b2d52865019d741bb69ca
vddCore <vddcore@outlook.com>
2023-01-12 11:35:20 +0100

    Added color output functionality.

--- drivers/tty/tty.c
+++ drivers/tty/tty.c
@@ -3,7 +3,6 @@
  * Description: Provides a TTY implementation.
  *
  * * * */
-
 #include <stddef.h>
 #include <string.h>
 
@@ -12,6 +11,7 @@
 
 static void put_char(char character);
 static void put_newline(vga_cursor_info_t *cursor_info);
+static vga_color_t determine_output_color(char character);
 
 void init_terminal(void) {
     initialize_screen_defaults();
@@ -26,7 +26,44 @@ void write(const char *string) {
     size_t length = strlen(string);
 
     for(size_t i = 0; i < length; i++) {
-        put_char(string[i]);
+        if(string[i] == '\\') {
+            if(i + 1 >= length) {
+                put_char(string[i]);
+            } else {
+                if(string[i + 1] == '\\') {
+                    i++;
+                    put_char(string[i]);
+                    continue;
+                }
+                i++;
+                switch(string[i]) {
+                    case '[':
+                        if(i + 1 >= length) {
+                            put_char(string[i]);
+                        } else {
+                            i++;
+                            set_vga_foreground(determine_output_color(string[i]));
+                        }
+                        break;
+                    case ']':
+                        if(i + 1 >= length) {
+                            put_char(string[i]);
+                        } else {
+                            i++;
+                            set_vga_background(determine_output_color(string[i]));
+                        }
+                        break;
+                    case 'X':
+                        reset_vga_colors();
+                        break;
+                    default:
+                        put_char(string[i]);
+                        break;
+                }
+            }
+        } else {
+            put_char(string[i]);
+        }
     }
 }
 
@@ -56,6 +93,45 @@ static void put_newline(vga_cursor_info_t *cursor_info) {
         cursor_info->y_pos += 1;
     } else {
         cursor_info->y_pos = VGA_HEIGHT - 1;
+    }
+}
+
+static vga_color_t determine_output_color(char character) {
+    switch(character) {
+        case '0':
+            return COLOR_BLACK;
+        case '1':
+            return COLOR_BLUE;
+        case '2':
+            return COLOR_GREEN;
+        case '3':
+            return COLOR_CYAN;
+        case '4':
+            return COLOR_RED;
+        case '5':
+            return COLOR_MAGENTA;
+        case '6':
+            return COLOR_BROWN;
+        case '7':
+            return COLOR_LIGHT_GRAY;
+        case '8':
+            return COLOR_DARK_GRAY;
+        case '9':
+            return COLOR_LIGHT_BLUE;
+        case 'A':
+            return COLOR_LIGHT_GREEN;
+        case 'B':
+            return COLOR_LIGHT_CYAN;
+        case 'C':
+            return COLOR_LIGHT_RED;
+        case 'D':
+            return COLOR_LIGHT_MAGENTA;
+        case 'E':
+            return COLOR_LIGHT_BROWN;
+        case 'F':
+            return COLOR_WHITE;
+        default:
+            return COLOR_BLACK;
     }
 }
 
